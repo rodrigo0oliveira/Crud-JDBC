ef4763b62bf5c03a23e43759c8166a192b41f40f
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-05-29 18:17:17 -0300

    Create class viewsAll

---
+++ src/views/All/Views.java
@@ -0,0 +1,16 @@
+package views.All;
+
+public class Views {
+	
+	public static String option() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Choose an option you want to check \n");
+		sb.append("\n");
+		sb.append("D) Department \n");
+		sb.append("S) Seller \n");
+		sb.append("O) Exit the sistem");
+		
+		return sb.toString();
+	}
+
+}
