fca2c39def353c79289657b30c3fe94faaa4d0c4
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-05-31 17:54:40 -0300

    Update entites

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -96,8 +96,8 @@ public class Seller implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", department=" + department + "]";
+		return "Vendedor Id =" + id + ", Nome =" + name + ", E-mail =" + email + ", Aniversário =" + birthDate + ", Salário ="
+				+ baseSalary + ", Departamento =" + department + "]";
 	}
 
 	
